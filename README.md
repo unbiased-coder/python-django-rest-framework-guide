54e29d3d11b8095e6db100975c773fda65a2c091
Unbiased Coder <info@unbiased-coder.com>
2021-10-12 21:45:30 +0300

    updating readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This has information related to my blog post on how to setup Django Rest Framewo
 
 If you would like to follow the guide please visit the full blog post on information here:
 
-https://unbiased-coder.com/how-to-setup-and-use-django-rest-framework-python-3/
+https://unbiased-coder.com/setup-django-rest-framework
--- app/serializers.py
+++ app/serializers.py
@@ -8,4 +8,21 @@ class PersonSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['first_name', 'last_name', 'dob']
 
 
+def print_name(first_name: str, last_name: str, age: int) -> str:
+    """[summary]
+
+    Args:
+        first_name (str): [description]
+        last_name (str): [description]
+        age (int): [description]
+
+    Returns:
+        str: [description]
+    """
+    a = 1
+    b = 2
+    if ((a==1) and (b == (a+1)) or (b+(a*2))>3):
+        print ('expression is true')
+
+    return 'I am %s %s and my age is: %d'%(first_name, last_name, age)
 
